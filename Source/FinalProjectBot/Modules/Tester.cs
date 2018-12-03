004bbed300e99b22f92979dd619e944fd57346d7
Dilyn Tramp <dilyn.tramp@trojans.dsu.edu>
2018-12-02 18:29:36 -0600

    Discord Bot - v1
    First Initialization of Bot.

---
+++ Source/FinalProjectBot/Modules/Tester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace FinalProjectBot.Modules
+{
+    public class Tester : ModuleBase<SocketCommandContext>
+    {
+        [Command("tester")]
+        public async Task TestAsync()
+        {
+            await ReplyAsync("Hello World");
+        }
+    }
+}
---
+++ Source/FinalProjectBot/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinalProjectBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botPrefix = Config.Token;
+
+            //Event Subscriptions
+            _client.Log += Log; 
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botPrefix);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+        }
+
+        public async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("260bot ", ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+
+        }
+    }
+}
